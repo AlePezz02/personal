e30c699e1a2fc51ab54dba91d5306fcc536bd1d9
AlePezz02 <alepezone02@gmail.com>
2024-12-11 11:18:40 +0100

    javascript

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let A = 10;
+/* let A = 10;
 const B = 10;
 
 console.log(A + B);
@@ -18,3 +18,20 @@ let outside = 80
     console.log(outside, inside)
 }
     console.log(outside, inside)
+ */
+   /*  let i = 0;
+
+    for (i ; i <= 20; i += 2){       //let i = 0; - La variabile i inizia da 0.
+        console.log(i)               //i <= 20; - Il ciclo continua finché i è minore o uguale a 20.
+    }                                //i += 2; - Dopo ogni iterazione, il valore di i aumenta di 2
+ */
+/* let start = 9
+
+    while (start >= 1) {
+        console.log(start)
+        start -= 2;
+    }
+ */
+    let word = prompt("parola");
+
+    console.log(word.length)
