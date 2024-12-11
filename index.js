9f3e041ffe29c65669ca113ebf8a09197f322582
AlePezz02 <alepezone02@gmail.com>
2024-12-11 12:33:04 +0100

    javascript

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ let outside = 80
 }
     console.log(outside, inside)
  */
-   /*  let i = 0;
+/*  let i = 0;
 
-    for (i ; i <= 20; i += 2){       //let i = 0; - La variabile i inizia da 0.
-        console.log(i)               //i <= 20; - Il ciclo continua finché i è minore o uguale a 20.
-    }                                //i += 2; - Dopo ogni iterazione, il valore di i aumenta di 2
- */
+ for (i ; i <= 20; i += 2){       //let i = 0; - La variabile i inizia da 0.
+     console.log(i)               //i <= 20; - Il ciclo continua finché i è minore o uguale a 20.
+ }                                //i += 2; - Dopo ogni iterazione, il valore di i aumenta di 2
+*/
 /* let start = 9
 
     while (start >= 1) {
@@ -32,6 +32,26 @@ let outside = 80
         start -= 2;
     }
  */
-    let word = prompt("parola");
+
+
+/*     let word = prompt("parola");
 
     console.log(word.length)
+ */
+
+
+/*     let parola;
+
+do {
+    parola = prompt("Inserisci una parola di almeno 5 caratteri:");
+} while (parola.length < 5);
+
+console.log("Hai inserito:", parola); */
+
+let number = 4
+
+if (number > 5) {
+    console.log("e maggiore di 5");
+}else if (number < 5) {
+    console.log("e minore di 5");
+}
