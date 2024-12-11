fd48bd6ce2bc38b39c5df420d5ed786c935a30ab
AlePezz02 <alepezone02@gmail.com>
2024-12-11 10:35:47 +0100

    javascript

---
+++ index.js
@@ -0,0 +1,20 @@
+let A = 10;
+const B = 10;
+
+console.log(A + B);
+
+let test = 5;
+console.log(test = test + 1);
+
+
+let number = 25
+    { let number = 15;
+    console.log(number)
+}
+
+let outside = 80
+{
+    let inside = 50
+    console.log(outside, inside)
+}
+    console.log(outside, inside)
